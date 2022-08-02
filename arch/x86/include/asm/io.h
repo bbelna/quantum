f3689c86d5134f111f2d28be7e4da3f5c4b6d36e
Brandon Belna <belnab@infinity-software.com>
2022-08-02 19:07:43 -0400

    output funcs

--- arch/x86/include/asm/io.h
+++ arch/x86/include/asm/io.h
@@ -46,3 +46,15 @@ inline static uint32_t inportl(uint16_t _port) {
   asm volatile("inl %1, %0":"=a"(rv):"dN"(_port));
   return rv;
 }
+
+inline static void outportb(uint16_t _port, uint8_t _data) {
+  asm volatile("outb %1, %0"::"dN"(_port), "a"(_data));
+}
+
+inline static void outportw(uint16_t _port, uint16_t _data) {
+  asm volatile("outw %1, %0"::"dN"(_port), "a"(_data));
+}
+
+inline static void outportl(uint16_t _port, uint32_t _data) {
+  asm volatile("outl %1, %0"::"dN"(_port), "a"(_data));
+}
